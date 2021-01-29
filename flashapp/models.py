a51d5a69203f51c3b822647f06f837f6f5697555
Trevor50 <trevornyagah4@gmail.com>
2021-01-29 09:16:27 +0300

    added information

--- flashapp/models.py
+++ flashapp/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from datetime import datetime
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     profile_photo = models.ImageField(upload_to="images/")
     email = models.EmailField(max_length=200)
+    member_since = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __str__(self):
         return self.user.username
